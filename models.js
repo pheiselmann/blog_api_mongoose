a16c30111df21a30fa0724026fe1c8be24737958
pheiselmann <prhmann@gmail.com>
2017-07-20 22:11:38 -0700

    alter files to conform with blog api specs

---
+++ models.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+// this is our schema to represent a blog entry
+const blogPostSchema = mongoose.Schema({
+  title: {type: String},
+  content: {type: String},
+  author: {
+    firstName: String,
+    lastName: String
+  }
+});
+
+// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
+// allow us to define properties on our object that manipulate
+// properties that are stored in the database. Here we use it
+// to generate a human readable string based on the author object
+// we're storing in Mongo.
+blogPostSchema.virtual('authorString').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim()});
+
+
+blogPostSchema.virtual('publishDate').get(function() {
+  return Date()});
+
+
+// this is an *instance method* which will be available on all instances
+// of the model. This method will be used to return an object that only
+// exposes *some* of the fields we want from the underlying data
+blogPostSchema.methods.apiRepr = function() {
+
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorString,
+    created: this.publishDate
+  };
+}
+
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const blogPost = mongoose.model('blogPost', blogPostSchema);
+
+module.exports = {blogPost};
